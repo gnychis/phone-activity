747e7974f82be2a5f01b0fd65f485ac64739b252
George Nychis <gnychis@gmail.com>
2012-06-29 20:02:54 -0400

    trying to create a service as a separate instance to the interface

--- src/com/authorwjf/ActivityService.java
+++ src/com/authorwjf/ActivityService.java
@@ -1,11 +1,8 @@
 package com.authorwjf;
 
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.Comparator;
 import java.util.List;
 
-import android.app.Activity;
+import android.app.Service;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
@@ -16,70 +13,36 @@ import android.hardware.SensorEvent;
 import android.hardware.SensorEventListener;
 import android.hardware.SensorManager;
 import android.net.wifi.ScanResult;
-import android.net.wifi.WifiConfiguration;
 import android.net.wifi.WifiManager;
-import android.os.Bundle;
-import android.view.View;
-import android.widget.ArrayAdapter;
-import android.widget.Spinner;
+import android.os.IBinder;
+import android.util.Log;
 import android.widget.Toast;
 
-import com.gnychis.PhoneActivity.R;
-
-public class Main extends Activity implements SensorEventListener {
+public class ActivityService extends Service implements SensorEventListener {
 	
+    public static Interface mMainActivity;
+
 	private float mLastX, mLastY, mLastZ;
 	private boolean mInitialized;
 	private SensorManager mSensorManager;
     private Sensor mAccelerometer;
     private final float NOISE = (float) 0.25;
+
     WifiManager wifi;
-    Spinner netlist;
-    Spinner agelist;
-    View theView;
-    List<ScanResult> scan_result;
     int user_is_home=0;
+    List<ScanResult> scan_result;
     String home_ssid;
-	 
-    Comparator<Object> netsort = new Comparator<Object>() {
-    	public int compare(Object arg0, Object arg1) {
-    		if(((WifiConfiguration)arg0).priority < ((WifiConfiguration)arg1).priority)
-    			return 1;
-    		else if( ((WifiConfiguration)arg0).priority > ((WifiConfiguration)arg1).priority)
-    			return -1;
-    		else
-    			return 0;
-    	}
-      };
     
-    /** Called when the activity is first created. */
     @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.main);
-        theView = findViewById(R.id.main_id);
+    public void onCreate() {
+    	super.onCreate();
+    	
         mInitialized = false;
         mSensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
         mAccelerometer = mSensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
         mSensorManager.registerListener(this, mAccelerometer , SensorManager.SENSOR_DELAY_NORMAL);
         
-        // Get the list of configured networks and add them to the spinner
         wifi = (WifiManager) getSystemService(Context.WIFI_SERVICE);
-        List<WifiConfiguration> cfgNets = wifi.getConfiguredNetworks();
-        netlist = (Spinner) findViewById(R.id.network_list);
-        ArrayList<String> spinnerArray = new ArrayList<String>();
-        Collections.sort(cfgNets,netsort);
-        for (WifiConfiguration config: cfgNets)
-        	spinnerArray.add(config.SSID.replaceAll("^\"|\"$", ""));
-        ArrayAdapter<String> spinnerArrayAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_item, spinnerArray);
-        spinnerArrayAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-        netlist.setAdapter(spinnerArrayAdapter);
-        
-        // Setup the age list
-        agelist = (Spinner) findViewById(R.id.age_group);
-        ArrayAdapter<CharSequence> ageAdapter = ArrayAdapter.createFromResource(this, R.array.age_ranges, android.R.layout.simple_spinner_item);
-        ageAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-        agelist.setAdapter(ageAdapter);
         
         // Create a broadcast receiver to listen for wifi scan results. We don't invoke them, we only passively
         // listen whenever they become available.
@@ -111,30 +74,30 @@ public class Main extends Activity implements SensorEventListener {
             		Toast.makeText(getApplicationContext(), "Got the boot complete...", Toast.LENGTH_LONG).show();	
             	}
             }
-        }, new IntentFilter("android.intent.action.BOOT_COMPLETED"));           
+        }, new IntentFilter("android.intent.action.BOOT_COMPLETED"));    
+        
+        if (mMainActivity != null)  Log.d(getClass().getSimpleName(), "ActivityService started");
     }
     
-    public void clickedFinished(View v) {
-    	
-    	// Get the string representations of the selected items
-    	home_ssid = (String) netlist.getSelectedItem();
-    	String selected_age = (String) agelist.getSelectedItem();
-
+    @Override
+    public void onDestroy() {
+    	super.onDestroy();
+    	mSensorManager.unregisterListener(this);
+    	if (mMainActivity != null)  Log.d(getClass().getSimpleName(), "ActivityService stopped");
     }
-
-    protected void onResume() {
-        super.onResume();
-        mSensorManager.registerListener(this, mAccelerometer, SensorManager.SENSOR_DELAY_NORMAL);
+    
+    @Override
+    public IBinder onBind(Intent intent) {
+    	return null;
     }
-
-    protected void onPause() {
-        super.onPause();
-        mSensorManager.unregisterListener(this);
-    }
-
+	
 	@Override
 	public void onAccuracyChanged(Sensor sensor, int accuracy) {
 		// can be safely ignored for this demo
+	}
+	
+	public static void setMainActivity(Interface activity) {
+		mMainActivity = activity;
 	}
 
 	@Override
@@ -159,14 +122,14 @@ public class Main extends Activity implements SensorEventListener {
 			mLastZ = z;
 			
 			if (deltaX > deltaY) {  // We moved horizontally
-				theView.setBackgroundColor(Color.RED);
+				mMainActivity.theView.setBackgroundColor(Color.RED);
 			} else if (deltaY > deltaX) {  // We moved vertically
-				theView.setBackgroundColor(Color.RED);
+				mMainActivity.theView.setBackgroundColor(Color.RED);
 			} else {
 				if(user_is_home==1)
-					theView.setBackgroundColor(Color.BLUE);
+					mMainActivity.theView.setBackgroundColor(Color.BLUE);
 				else
-					theView.setBackgroundColor(Color.BLACK);
+					mMainActivity.theView.setBackgroundColor(Color.BLACK);
 			}
 		}
 	}
---
+++ src/com/authorwjf/Interface.java
@@ -0,0 +1,85 @@
+package com.authorwjf;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
+
+import android.app.Activity;
+import android.content.Context;
+import android.content.Intent;
+import android.net.wifi.WifiConfiguration;
+import android.net.wifi.WifiManager;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+
+import com.gnychis.PhoneActivity.R;
+
+public class Interface extends Activity {
+	
+    Spinner netlist;
+    Spinner agelist;
+    View theView;
+    String home_ssid;
+    WifiManager wifi;
+	 
+    Comparator<Object> netsort = new Comparator<Object>() {
+    	public int compare(Object arg0, Object arg1) {
+    		if(((WifiConfiguration)arg0).priority < ((WifiConfiguration)arg1).priority)
+    			return 1;
+    		else if( ((WifiConfiguration)arg0).priority > ((WifiConfiguration)arg1).priority)
+    			return -1;
+    		else
+    			return 0;
+    	}
+      };
+    
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+    	
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+        theView = findViewById(R.id.main_id);
+        
+        // Create the service that runs in the background and captures the activity data
+        final Intent activityService = new Intent(this, ActivityService.class);
+        startService(activityService);
+        
+        // Get the list of configured networks and add them to the spinner
+        wifi = (WifiManager) getSystemService(Context.WIFI_SERVICE);
+        List<WifiConfiguration> cfgNets = wifi.getConfiguredNetworks();
+        netlist = (Spinner) findViewById(R.id.network_list);
+        ArrayList<String> spinnerArray = new ArrayList<String>();
+        Collections.sort(cfgNets,netsort);
+        for (WifiConfiguration config: cfgNets)
+        	spinnerArray.add(config.SSID.replaceAll("^\"|\"$", ""));
+        ArrayAdapter<String> spinnerArrayAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_item, spinnerArray);
+        spinnerArrayAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        netlist.setAdapter(spinnerArrayAdapter);
+        
+        // Setup the age list
+        agelist = (Spinner) findViewById(R.id.age_group);
+        ArrayAdapter<CharSequence> ageAdapter = ArrayAdapter.createFromResource(this, R.array.age_ranges, android.R.layout.simple_spinner_item);
+        ageAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        agelist.setAdapter(ageAdapter);       
+    }
+    
+    public void clickedFinished(View v) {
+    	// Get the string representations of the selected items
+    	home_ssid = (String) netlist.getSelectedItem();
+    	String selected_age = (String) agelist.getSelectedItem();
+    }
+
+    protected void onResume() {
+        super.onResume();
+    }
+
+    protected void onPause() {
+        super.onPause();
+        
+    }
+
+}
